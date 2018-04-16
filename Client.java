fcfba16c8f1ed2147bf0c20e9db3c1786a877629
JBuursink <31792727+JBuursink@users.noreply.github.com>
2018-04-16 23:49:44 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Socket s = new Socket("localhost", 4711);
 		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-		out.print("teslthjkathklas");
+		out.print("tekst");
 		out.flush();
 		// Socket s = new Socket("145.89.166.28", 4711);
 		s.close();
