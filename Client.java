94bf848c8bd9493d1d946bbceda993891497dc8d
JBuursink <31792727+JBuursink@users.noreply.github.com>
2018-04-16 22:43:55 +0200

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,16 @@
+package les1.practicum2;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Socket s = new Socket("localhost", 4711);
+		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+		out.print("teslthjkathklas");
+		out.flush();
+		// Socket s = new Socket("145.89.166.28", 4711);
+		s.close();
+	}
+
+}
